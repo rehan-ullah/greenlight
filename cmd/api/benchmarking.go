000d2a22eaf9cb3151fc1f34ee2487b0067f3526
rehan-ullah <rehanu2k16@gmail.com>
2024-06-10 00:22:20 +0400

    struct json encoding

---
+++ cmd/api/benchmarking.go
@@ -0,0 +1,33 @@
+package main
+
+// func BenchmarkEncoder(b *testing.B) {
+// 	w := httptest.NewRecorder()
+// 	r := new(http.Request)
+// 	for n := 0; n < b.N; n++ {
+// 		healthcheckHandlerEncoder(w, r)
+// 	}
+// }
+
+// func BenchmarkMarshal(b *testing.B) {
+// 	w := httptest.NewRecorder()
+// 	r := new(http.Request)
+// 	for n := 0; n < b.N; n++ {
+// 		healthcheckHandlerMarshal(w, r)
+// 	}
+// }
+
+// func BenchmarkMarshalIndent(b *testing.B) {
+// 	w := httptest.NewRecorder()
+// 	r := new(http.Request)
+// 	for n := 0; n < b.N; n++ {
+// 		healthcheckHandlerMarshalIndent(w, r)
+// 	}
+// }
+
+// func BenchmarkMarshal(b *testing.B) {
+// 	w := httptest.NewRecorder()
+// 	r := new(http.Request)
+// 	for n := 0; n < b.N; n++ {
+// 		healthcheckHandlerMarshal(w, r)
+// 	}
+// }
